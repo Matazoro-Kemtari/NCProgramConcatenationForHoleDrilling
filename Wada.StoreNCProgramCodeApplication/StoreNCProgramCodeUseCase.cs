41477d49ed2bfe3e58e84efa6615be097cef2c7c
kondara-mach <k_hirano@wadass.com>
2023-01-24 17:07:35 +0900

    保存のオブジェクト作成

---
+++ Wada.StoreNCProgramCodeApplication/StoreNCProgramCodeUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Wada.UseCase.DataClass;
+
+namespace Wada.StoreNCProgramCodeApplication
+{
+    public interface IStoreNCProgramCodeUseCase
+    {
+        Task ExecuteAsync(NCProgramCodeAttempt storableCode);
+    }
+
+    public class StoreNCProgramCodeUseCase : IStoreNCProgramCodeUseCase
+    {
+        public Task ExecuteAsync(NCProgramCodeAttempt storableCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
