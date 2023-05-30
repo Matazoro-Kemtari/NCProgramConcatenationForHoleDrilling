5e1b16143603d4fd8de3ac1d8fba2ab2e3da58f0
Matazoro-Kemtari <k_hirano@wadass.com>
2023-05-30 11:50:07 +0900

    ハイライト表示対応

--- Wada.NcProgramConcatenationForHoleDrilling/Models/PreviewPageModel.cs
+++ Wada.NcProgramConcatenationForHoleDrilling/Models/PreviewPageModel.cs
@@ -1,9 +1,27 @@
-﻿using Reactive.Bindings;
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using Reactive.Bindings;
+using System.Reflection;
+using System.Xml;
+using System;
+using ICSharpCode.AvalonEdit.Document;
 
 namespace Wada.NcProgramConcatenationForHoleDrilling.Models
 {
     internal record class PreviewPageModel
     {
-        internal ReactivePropertySlim<string> CombinedProgramSource { get; } = new();
+        public PreviewPageModel()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using var xshd = assembly.GetManifestResourceStream("Wada.NcProgramConcatenationForHoleDrilling.ViewModels.NcHighlighting.xshd")
+            ?? throw new InvalidOperationException("Could not find embedded resource");
+
+            using XmlReader reader = new XmlTextReader(xshd);
+
+            NcHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+        }
+
+        internal ReactivePropertySlim<TextDocument> CombinedProgramSource { get; } = new();
+        public IHighlightingDefinition NcHighlighting { get; }
     }
 }
